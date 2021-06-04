4bad83ab51df8a2335c7bd705ac195cff03a093d
Ben Tranter <ben@bentranter.io>
2021-06-04 11:20:56 -0400

    Add LSP config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -18,6 +18,8 @@ require("packer").startup({
         require("gitsigns").setup()
       end
     }
+    use "neovim/nvim-lspconfig"
+    use "nvim-lua/completion-nvim"
   end
 })
 
@@ -66,3 +68,26 @@ vim.g.go_fmt_command = "goimports" -- Run GoImports on save.
 vim.g.go_auto_type_info = 1        -- Show type info for symbol under cursor.
 vim.g.go_fmt_fail_silently = 1     -- Don't open the quickfix window.
 vim.g.go_def_mode = "godef"        -- Override because gopls breaks sometimes.
+
+-- LSP config.
+local nvim_lsp = require("lspconfig")
+local on_attach = function(_, bufnr)
+  require("completion").on_attach()
+end
+
+local servers = { "gopls" }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach
+  }
+end
+
+vim.cmd [[
+  " Recommended autocompletion settings for completion-nvim.
+  " Use <Tab> and <S-Tab> to navigate through popup menu
+  inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
+  inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
+
+  " Avoid showing message extra message when using completion
+  set shortmess+=c
+]]
